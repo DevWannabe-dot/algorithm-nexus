0695a95c650315d2dcb2f76db269aba5fa8ac2ad
Pedro H <pedro.26.3.h@gmail.com>
2023-12-27 15:40:02 -0300

    Draw and output between argv[2] numbers, argv[1] times.

---
+++ generateMegaSena.c
@@ -0,0 +1,42 @@
+/**
+ * @file    generateMegaSena.c
+ * @brief   Generates six numbers from 1 to 60 for the "Mega-Sena" raffle if no arguments are passed. Otherwise, draws between argv[2] numbers argv[1] times.
+ * @author  Pedro Henrique Pinto de Oliveira
+ * @date    2023-12-27
+*/
+
+/* Inclusions */
+#include "headers/errors.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Constantes */
+#define N_NUMEROS_SORTEADOS     (6)
+#define N_NUMEROS_POSSIVEIS     (60)
+
+/* Tipos */
+
+/* Funções */
+int main(int argc, char ** argv) {
+    srand(time(NULL));
+
+    if(argc == NO_ARGS_PROVIDED){
+        for(int i = 0; i < N_NUMEROS_SORTEADOS; i++) {
+            int nAtual = (rand() % N_NUMEROS_POSSIVEIS) + 1;
+
+            printf("%d", nAtual);
+            if(i != N_NUMEROS_SORTEADOS-1)
+                { printf(" "); }
+        }
+    } else if(argc == NO_ARGS_PROVIDED + 2) {
+        for(int i = 0; i < (int)strtod(argv[1], NULL); i++) {
+            int nAtual = (rand() % (int)strtod(argv[2], NULL)) + 1;
+
+            printf("%d", nAtual);
+            if(i != (int)strtod(argv[1], NULL)-1)
+                { printf(" "); }
+        }
+    }
+    return SUCESSO;
+}
