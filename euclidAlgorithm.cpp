d80b93b8f596eba3063a79ecd6d80772d5a656ba
Pedro Henrique <pedro.26.3.h@gmail.com>
2024-07-21 14:00:13 -0300

    Run xorSwap() independently, rename files

--- euclidAlgorithm.cpp
+++ euclidAlgorithm.cpp
@@ -1,5 +1,5 @@
 /**
- * @file    euclid_algorithm.cpp
+ * @file    euclidAlgorithm.cpp
  * @brief   Receives two numbers (argv[1], argv[2]) to compute their GCD/GCF/HCF.
  * @author  Pedro Henrique Pinto de Oliveira
  * @date    2024-07-19
@@ -9,7 +9,7 @@
 #include "headers/errors.h"
 #include "headers/xor_swap.h"
 #include <iostream>
-#include <cstdlib>
+#include <cmath>
 using namespace std;
 
 /* Constants */
@@ -32,10 +32,10 @@ long euclidCalcGDC(long a, long b)
 {
     long GDC = 1;
 
-    if(a < b) xorSwap(&a, &b); // can't divide a integer by a greater integer and get quotient > 0
+    if(abs(a) < abs(b)) xorSwap(&a, &b); // can't divide a integer by a greater integer and get quotient > 0
     if(a == b) return a; // mdc(a, a) = ax + ay (Bézout) = a^2(x+y) | a \forall a \therefore (a, a) = a.
 
-    GDC = euclidAlgo(a, b);
+    GDC = euclidAlgo(abs(a), abs(b)); // absolute value needed for recursive implementation
 
     return GDC;
 }
@@ -48,7 +48,7 @@ int main(int argc, char * argv[])
     a = strtol(argv[1], nullptr, 0);
     b = strtol(*(&argv[1] + sizeof(char)), nullptr, 0);
 
-    cout << euclidCalcGDC(a, b);
+    cout << euclidCalcGDC(a, b) << '\n';
     
     return SUCCESS;
 }
--- xor_swap.c
+++ xor_swap.c
@@ -11,6 +11,45 @@
 #include <stdlib.h>
 #include <math.h>
 
+void xorSwap(long* a, long* b)
+{
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+int_bin *  evaluate_Base10ToBase2(int Base10_N, int_bin * Base2_N)
+{
+    // Allow for numbers between [-2^31, 2^31 -1] inclusive
+    if(Base10_N < 0) Base2_N[0] = 1;
+
+    for(int i = INTMAX_MAX_32B_EXP; i > 0; i--){
+        int remainder = Base10_N % 2;
+        
+        Base2_N[i] = remainder;
+        Base10_N = (Base10_N - remainder) / 2;
+    }
+    
+    return Base2_N;     
+}
+void print_Base10ToBase2(int Base10_N)
+{
+    int_bin * Base2_N = (int_bin*) calloc((INTMAX_MAX_32B_EXP+1), sizeof(int_bin));
+    int foundFirstHIGH = 0;
+
+    Base2_N = evaluate_Base10ToBase2(Base10_N, Base2_N);
+    
+    if(Base2_N[0] == 1) printf("-");
+    for(int i = 1; i < (INTMAX_MAX_32B_EXP+1); i++){
+        if(!foundFirstHIGH) {
+            // Try:
+            foundFirstHIGH |= Base2_N[i];
+            // If it's still not found, skip this iteration
+            if(!foundFirstHIGH) continue;
+        }
+        printf("%d", Base2_N[i]);
+    }
+}
+#ifndef EXTERN_XOR_SWAP
 int main(int argc, char ** argv)
 {
     if(argc != NO_ARGS_PROVIDED + 2) return NOT_ENOUGH_ARGS;
@@ -33,3 +72,4 @@ int main(int argc, char ** argv)
 
     return SUCCESS;
 }
+#endif
